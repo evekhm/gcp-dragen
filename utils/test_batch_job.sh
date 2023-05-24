45926150ced15e6d0dbdb3cb168b37665744f1f2
evekhm <evekhm@gmail.com>
2023-05-24 13:45:45 -0700

    - Skeleton of a working job

--- utils/test_batch_job.sh
+++ utils/test_batch_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 date_str=$(date +%s )
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source "${DIR}"/SET
+source "${DIR}"/../SET
 JOB_NAME_UID="projects/${PROJECT_ID}/locations/${GCLOUD_REGION}/jobs/test-batch-${date_str}"
 echo "Submitting $JOB_NAME_UID"
 gcloud beta batch jobs submit "$JOB_NAME_UID" --network="projects/$PROJECT_ID/global/networks/$GCLOUD_NETWORK" --subnetwork="projects/$PROJECT_ID/regions/${GCLOUD_REGION}/subnetworks/$GCLOUD_SUBNET" --config - <<EOD
@@ -35,7 +35,7 @@ gcloud beta batch jobs submit "$JOB_NAME_UID" --network="projects/$PROJECT_ID/gl
     ],
     "allocationPolicy": {
       "serviceAccount": {
-          "email": "${SA_JOB_EMAIL}"
+          "email": "${JOB_SERVICE_ACCOUNT}"
       },
       "instances": [
         {
