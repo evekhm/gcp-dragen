2e69c209c4cd372d3e2c4df9e0b5ea6e18fc7a19
evekhm <evekhm@gmail.com>
2023-08-03 18:52:22 -0700

    - Fix env

--- run_cram_403.sh
+++ run_cram_403.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "${DIR}"/SET
 
 #bash -e "${DIR}/utils/get_configs.sh" | tee -a "$LOG"
 gsutil cp "${DIR}"/cloud_function/START_PIPELINE gs://"${INPUT_BUCKET_NAME}/cram_test/403/"
